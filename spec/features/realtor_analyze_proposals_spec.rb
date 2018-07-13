e239b844427d0c7e65daa07144a863c0bb72d6e9
Marcos Vieira / Rafael Abreu <web.nogs@gmail.com>
2018-07-13 19:44:20 -0300

    feat(Visualizar todas as propostas)

--- spec/features/realtor_analyze_proposals_spec.rb
+++ spec/features/realtor_analyze_proposals_spec.rb
@@ -9,9 +9,9 @@ feature 'View all proposal pendent' do
   end
 
   scenario 'successfully' do
-    region = Region.create(name: 'São Paulo')
-    property_type = PropertyType.create(name: 'Apartamento')
-    property = Property.create(title: 'Casa Chic', 
+    region = Region.create!(name: 'São Paulo')
+    property_type = PropertyType.create!(name: 'Apartamento')
+    property = Property.create!(title: 'Casa Chic', 
                     room_quantity: 4, 
                     maximum_guests: 5, 
                     minimum_rent: 2, 
@@ -20,7 +20,7 @@ feature 'View all proposal pendent' do
                     property_type: property_type,
                     region: region,
                     photo: File.new(Rails.root.join('spec', 'support', 'casa.jpg')))
-    proposal = Proposal.create(start_date: '2018-01-01',
+    proposal = Proposal.create!(start_date: '2018-01-01',
                                end_date: '2018-01-30',
                                total_amount: 200.00,
                                total_guests: 4,
@@ -31,7 +31,7 @@ feature 'View all proposal pendent' do
                                pet: true,
                                smoker: true,
                                details: 'Muito bom',
-                               property_id: property)
+                               property: property)
 
 
     visit root_path

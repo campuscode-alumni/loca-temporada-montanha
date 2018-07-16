7ddd28e952906022a0ebf44d2f0f776fff17bf53
Marcos Vieira <mavbvieira@gmail.com>
2018-07-16 01:35:11 -0300

    Adiciona a Editção nos imoveis

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :realtors
   root to: 'home#index'
-  resources :properties, only: [:show, :new, :create] do
+  resources :properties, only: [:show, :new, :create, :index, :edit, :update] do
     resources :proposals, only: [:new, :create, :show]
   end
   resources :property_types, only: [:show, :new, :create]

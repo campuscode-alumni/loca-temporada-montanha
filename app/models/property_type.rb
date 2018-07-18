c53681be1fd2579b39cd1ae845961d3e9a1f7967
Marcos e Marcos <mavmaso@gmail.com>
2018-07-17 21:21:57 -0300

    Adicionar edição de regiões, tipos de imóveis e imóveis

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -2,4 +2,6 @@ class PropertyType < ApplicationRecord
   has_many :properties
 
   validates :name, presence: { message: 'não pode ficar em branco' }
+  validates :name, uniqueness: { case_sensitive: false,
+                                message: 'Tipo de imóvel já cadastrado'}
 end
